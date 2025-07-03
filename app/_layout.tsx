7dd00b246245c252163bef91dbd8e9b3b5b3797a
LiAdonis11 <127942925+LiAdonis11@users.noreply.github.com>
2025-07-04 01:47:03 +0800

    Added SafeAreaProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from 'expo-router'
 import { TamaguiProvider } from 'tamagui'
 import tamaguiConfig from '../tamagui.config'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function Layout() {
   return (
-    <TamaguiProvider config={tamaguiConfig}>
-      <Stack />
-    </TamaguiProvider>
+    <SafeAreaProvider>
+      <TamaguiProvider config={tamaguiConfig}>
+        <Stack />
+      </TamaguiProvider>
+    </SafeAreaProvider>
   )
 }
